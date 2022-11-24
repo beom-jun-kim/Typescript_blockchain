64094f7d685355ffc83eee92585bc7a341bff211
kimbeomjun <gimb52043@gmail.com>
2022-11-24 12:53:47 +0900

    Typescript_declaration_files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-document.querySelector()
+// 정의 파일은 자바스크립트 코드와 모양을 타입스크립에 설명해주는 파일이다
+// 타입스크립트는 작성자가 쓰는 모든것에 대한 타입을 이해하고 있어야 한다
+// myPackage.d.ts라는 정의파일에서 타입 정해주면 import가 된다
+
+import { init, exit } from "myPackage";
+
+init({
+    url:"true"
+})
+
+exit(1)
+
+localStorage.clear()
